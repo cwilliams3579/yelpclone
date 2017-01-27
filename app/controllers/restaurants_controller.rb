3665d06020f9f0236e8b6ea3b91cf00e9c2272ae
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-01-27 09:58:57 -0600

    added images with carrierwave

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,8 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
-
+  expose :restaurants, ->{ Restaurant.all }
+  expose :restaurant
+  
   def index
     @restaurants = Restaurant.all
   end
@@ -62,6 +64,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone, :website)
+      params.require(:restaurant).permit(:name, :address, :phone, :website, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127024712) do
+ActiveRecord::Schema.define(version: 20170127055730) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170127024712) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
