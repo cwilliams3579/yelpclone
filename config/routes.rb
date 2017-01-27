85efe3eeb191c8132a26d5296369ee86f459a115
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-01-27 12:43:11 -0600

    add sign in links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :restaurants
-  get 'pages/about'
 
+  get 'pages/about'
   get 'pages/contact'
   post 'restaurants' => 'restaurants#new'
 
