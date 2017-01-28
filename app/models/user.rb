2d7d2c7fa387fee286250bbd569de9a3d96a29ff
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-01-27 22:39:36 -0600

    add data vaildations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+  validates :first_name, :last_name, presence: true, length: { in: 3..20 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+  before_save do
+    self.email.downcase!
+  end
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 end
